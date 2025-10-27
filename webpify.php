6edd38f4be7f01b6f58a6ae4055e5bbb0a351a96
youssef bouhlal <pro.youssef.bouhlal@gmail.com>
2025-10-27 22:35:47 +0100

    update the PHP version required

--- webpify.php
+++ webpify.php
@@ -5,8 +5,8 @@
  * Description:       Give your WordPress site a performance boost — effortlessly convert and serve modern image formats (WebP & AVIF) for faster pages and better SEO.
  * Tags:              image optimization, webp, avif, image converter, performance, speed, seo, image compression, next-gen images, WordPress optimization, media, convert images, image formats, web performance
  * Version:           1.0.0
- * Requires at least: 6.0
- * Requires PHP:      7.4
+ * Requires at least: 6.8
+ * Requires PHP:      8.1
  * Author:            Youssef Bouhlal
  * Author URI:        pro.youssef.bouhlal@gmail.com
  * License:           GPLv2 or later
@@ -29,8 +29,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 const VERSION             = '1.0.0';
 const PLUGIN_FILE         = __FILE__;
 const PLUGIN_REQUIREMENTS = array(
-	'php_version' => '7.4',
-	'wp_version'  => '6.0',
+	'php_version' => '8.1',
+	'wp_version'  => '6.8',
 );
 
 /**
