7e839cfbf8212d5558217d9223485a4161b9066e
youssef bouhlal <pro.youssef.bouhlal@gmail.com>
2025-11-08 23:05:20 +0100

    generate required files for test with wp-cli

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Webpify
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
